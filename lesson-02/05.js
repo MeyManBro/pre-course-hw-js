69187578ab3d52cf3c8e64b1b422d13dea09be05
MeyManBro <alexxlefa@gmail.com>
2023-11-21 15:06:23 +0300

    lesson 2 edits task 5

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,9 +2,11 @@ let passport = {
   name: 'Petr',
   surname: 'Petrov',
 };
+
+console.log(passport);
+
 let passportNew = {};
 Object.assign(passportNew, passport);
 passportNew.name = 'Ivan';
 
-console.log(passport);
 console.log(passportNew);
