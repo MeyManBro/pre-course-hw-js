4ba8f83584bc8d6095fc005e83de6cbffef6c810
MeyManBro <alexxlefa@gmail.com>
2023-12-03 17:45:42 +0300

    03 lesson task 1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,7 +1,7 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-  return colors ФИЛЬТРУЕМ.ПРЕВРАЩАЕМ В СТРОКУ('-')
+  return colors.filter(el => (el === 'черный' || el === 'красный' || el === 'желтый')).join('-')
 }
 
 console.log(createColorString());
