1601772761a2a01fe88c5a71d3e11fe43d71f402
MeyManBro <alexxlefa@gmail.com>
2023-12-03 18:33:19 +0300

    03 lesson task 2

---
+++ lesson-03/02.js
@@ -0,0 +1,12 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+
+function searchOrange() {
+  if (words.indexOf("апельсин") === 2) {
+    return console.log('«Ура! нашел»');
+  } else {
+    return console.log('«Придется поискать в другом магазине…»');
+  }
+}
+
+searchOrange();
+
